ad289791092693483f6d368536ee9eeb8fb013fb
Keshav Mathur <keshav37a@gmail.com>
2020-01-26 23:24:17 +0530

    added playlist UI

--- js/musicControls.js
+++ js/musicControls.js
@@ -2,6 +2,7 @@ let musicControlDiv = document.querySelector('.music-player')
 let latestReleaseItem = document.querySelector('.latest-release-single')
 let latestEnglishItem = document.querySelector('.latest-english')
 let latestHindiItem = document.querySelector('.latest-hindi')
+// let songItem = document.querySelector('.song-item')
 
 latestReleaseItem.addEventListener('click', e=>{
     console.log('openPlayerOnClickCalled');
@@ -20,6 +21,12 @@ latestHindiItem.addEventListener('click', e=>{
     musicControlDiv.classList.remove('player-hidden')
     console.log("musicControlDiv", musicControlDiv);
 })
+
+// songItem.addEventListener('click', e=>{
+//     console.log('songItem called');
+//     musicControlDiv.classList.remove('player-hidden')
+// })
+
 
 // console.log("musicControlDiv", musicControlDiv);
 // const openPlayerOnClick = () =>{
